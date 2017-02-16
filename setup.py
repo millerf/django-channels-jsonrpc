111f70ff8702982c1c4c724b1a6a2b40641e6fae
Dmytro Kostochko <alerion.um@gmail.com>
2017-02-16 13:44:20 +0200

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-channels-jsonrpc',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages(),
     install_requires=[
           'channels',
